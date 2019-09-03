7d4c82a22c5eba342aca27974a2b584dc15fd2ee
15715198163 <lovelynch@163.com>
2019-09-03 11:27:22 +0800

    update

--- src/com/tencent/bishi91/Main3.java
+++ src/com/tencent/bishi91/Main3.java
@@ -9,6 +9,12 @@ import java.util.Scanner;
  * <p>
  * <p>
  * 5
+ * 如果我们只有一个人的话，那么需要sum(A)+n的时间完成任务。所以我们可以遍历数组A计算前缀和cnt，如果cnt+i>=x（i表示遍历到的A中元素位置），说明此时人手不够了，我们需要增加人手。当我们增加一个人手后，我们相应的消耗时间就要减少x-i（其中x表示我们的目标时间，在位置i，每个人有x-i时间搬物品）。当最后所需人手个数小于m或者人手恰好是m且前缀合cnt小于等于0的时候，说明在x时间内可行，否则不可行。
+ * <p>
+ * 接着就是二分法的常规套路，当我们发现当前的mid可行的时候，我们的r=mid，否则l=mid+1。
+ * ————————————————
+ * 版权声明：本文为CSDN博主「coordinate_blog」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
+ * 原文链接：https://blog.csdn.net/qq_17550379/article/details/100355642
  **/
 public class Main3 {
     public static void main(String[] args) {
@@ -20,10 +26,49 @@ public class Main3 {
             boxNum[i] = input.nextInt();
 
         }
-        System.out.println(getMinTime(m, boxNum));
+        System.out.println(getMinTime(m, n, boxNum));
     }
 
-    private static int getMinTime(int m, int[] boxNum) {
-        return 0;
+    private static int getMinTime(int m, int n, int[] boxNum) {
+        int minTime = 0;
+        int sumBox = 0;
+        for (int box : boxNum)
+            sumBox += box;
+        if (m > sumBox)
+            return (n + 1);
+        else {
+            int left = 2;
+            int right = n + sumBox;
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (checkAdd(mid, n, m, boxNum)) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            minTime = right;
+        }
+        return minTime;
     }
+
+    private static boolean checkAdd(int mid, int n, int m, int[] boxNum) {
+        int count = 0, people = 0;
+        for (int i = 1; i < n + 1; i++) {
+            count += boxNum[i - 1];
+            if (i >= mid)
+                return false;
+            while (i + count > mid) {
+                count -= mid - i;
+                people++;
+                if (people > m)
+                    return false;
+            }
+        }
+        if (people == m)
+            return count <= 0;
+        return true;
+
+    }
+
 }
--- src/com/tencent/bishi91/Main5.java
+++ src/com/tencent/bishi91/Main5.java
@@ -15,27 +15,49 @@ import java.util.Scanner;
  * 6
  * 5
  * 5
+ * 定义函数f(i)表示长度为i的时候合法方案数。那么
+ * <p>
+ * f(i)=f(i−1)+f(i−k)
+ * f(i) 的方案数只能来源于两种情况：一种是前面的白花不连续，一种是白花连续。
+ * 版权声明：本文为CSDN博主「coordinate_blog」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
+ * 原文链接：https://blog.csdn.net/qq_17550379/article/details/100355642
  **/
 public class Main5 {
+    public static final int MAX = (int) (1e5 + 5);
+    public static final int MOD = (int) (1e5 + 7);
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int t = input.nextInt();
         int k = input.nextInt();
-        int[][] locationLength = new int[t][];
+        int[][] locationLength = new int[t][2];
         for (int i = 0; i < t; i++) {
             locationLength[i][0] = input.nextInt();
             locationLength[i][1] = input.nextInt();
 
         }
 
-        List<Integer> results = getSolution(locationLength, k);
-        for (int result : results)
+        List<Long> results = getSolution(locationLength, k);
+        for (long result : results)
             System.out.println(result);
 
     }
 
-    private static List<Integer> getSolution(int[][] locationLength, int k) {
-        List<Integer> results = new ArrayList<>();
+    private static List<Long> getSolution(int[][] locationLength, int k) {
+        List<Long> results = new ArrayList<>();
+        long[] dp = new long[MAX];
+        long[] sum = new long[MAX];
+        for (int i = 0; i < k; i++)
+            dp[i] = 1;
+        for (int i = k; i < MAX; i++)
+            dp[i] = (dp[i - 1] % MOD + dp[i - k] % MOD) % MOD;
+        sum[1] = dp[1];
+        for (int i = 2; i < MAX; i++)
+            sum[i] = (sum[i - 1] % MOD + dp[i] % MOD) % MOD;
+        for (int i = 0; i < locationLength.length; i++) {
+            results.add((MOD + sum[locationLength[i][1]] - sum[locationLength[i][0] - 1]) % MOD);
+        }
+
         return results;
     }
 }
